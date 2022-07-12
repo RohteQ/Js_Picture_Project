ba07d8d3c945b767cdde74e9a4859db4bd540d0d
Andrew Afanasiev <rohteq@gmail.com>
2022-07-12 03:12:03 +0300

    changed path

--- dist/script.js
+++ dist/script.js
@@ -225,7 +225,7 @@ const clearObject = state => {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 const drop = () => {
-  const fileInputs = document.querySelectorAll('[name="upload]'); //massive of events
+  const fileInputs = document.querySelectorAll('[name="upload"]'); //massive of events
 
   ['dragenter', 'dragleave', 'dragover', 'drop'].forEach(eventName => {
     fileInputs.forEach(input => {
@@ -244,20 +244,22 @@ const drop = () => {
   }
 
   function unhighlight(item) {
-    item.closest('.file_upload').style.border = 'none';
+    item.closest('.file_upload').style.border = "none";
 
     if (item.closest('.calc_form')) {
-      item.closest('.file_upload').style.backgroundColor = '#fff';
+      item.closest('.file_upload').style.backgroundColor = "#fff";
     } else {
-      item.closest('.file_upload').style.backgroundColor = '#ededed';
+      item.closest('.file_upload').style.backgroundColor = "#ededed";
     }
 
-    item.closest('.file_upload').style.backgroundColor = '#ededed';
+    if (item.closest('.main')) {
+      item.closest('.file_upload').style.backgroundColor = '#f7e7e6';
+    }
   } //2 events 
 
 
   ['dragenter', 'dragover'].forEach(eventName => {
-    //enumeration all file inputs we work with, 
+    //enumeration all file inputs we work with
     fileInputs.forEach(input => {
       // on every input add event and set the handler
       input.addEventListener(eventName, () => highlight(input), false);
@@ -268,14 +270,42 @@ const drop = () => {
       input.addEventListener(eventName, () => unhighlight(input), false);
     });
   });
+
+  const clearInput = () => {
+    const unloadedFile = document.querySelector('.file-unloaded');
+    fileInputs.forEach(unloadedFile => {
+      unloadedFile.value = '';
+    });
+    fileInputs.forEach(unloadedFile => {
+      unloadedFile.previousElementSibling.textContent = "Файл не выбран";
+    });
+  };
+
   fileInputs.forEach(input => {
     input.addEventListener('drop', e => {
       input.files = e.dataTransfer.files;
       let dots;
       const arr = input.files[0].name.split('.');
-      arr[0].length > 6 ? dots = '...' : dots = '.';
+      arr[0].length > 6 ? dots = "..." : dots = '.';
       const name = arr[0].substring(0, 6) + dots + arr[1];
       input.previousElementSibling.textContent = name;
+
+      if (input.closest('main')) {
+        const formData = new FormData();
+        formData.append('file', input.files[0]);
+
+        const postData = async (url, data) => {
+          let res = await fetch(url, {
+            method: "POST",
+            body: data
+          });
+          return await res.text();
+        };
+
+        postData('assets/server.php', formData).then(res => console.log(res)).catch(() => console.log('Ошибка'));
+      }
+
+      clearInput();
     });
   });
 };
